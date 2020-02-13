ae13c535db99f446b38afee78fbc77c96d23e719
souren <souren@papazian.info>
2020-02-13 17:11:09 -0500

    Measuring Performance project

---
+++ Measuring Performance/Assets/FPSCounter.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class FPSCounter : MonoBehaviour
+{
+
+    public int AverageFPS { get; private set; }
+    public int frameRange = 60;
+
+    int[] fpsBuffer;
+    int fpsBufferIndex;
+
+    void InitializeBuffer()
+    {
+        if (frameRange <= 0)
+        {
+            frameRange = 1;
+        }
+        fpsBuffer = new int[frameRange];
+        fpsBufferIndex = 0;
+    }
+
+    void Update()
+    {
+        if (fpsBuffer == null || fpsBuffer.Length != frameRange)
+        {
+            InitializeBuffer();
+        }
+        UpdateBuffer();
+        CalculateFPS();
+    }
+
+    void UpdateBuffer()
+    {
+        fpsBuffer[fpsBufferIndex++] = (int)(1f / Time.unscaledDeltaTime);
+        if (fpsBufferIndex >= frameRange)
+        {
+            fpsBufferIndex = 0;
+        }
+    }
+
+    void CalculateFPS()
+    {
+        int sum = 0;
+        for (int i = 0; i < frameRange; i++)
+        {
+            sum += fpsBuffer[i];
+        }
+        AverageFPS = sum / frameRange;
+    }
+}
---
+++ Measuring Performance/Assets/Nucleon.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Nucleon : MonoBehaviour
+{
+
+    public float attractionForce;
+
+    Rigidbody body;
+
+    void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        body.AddForce(transform.localPosition * -attractionForce);
+    }
+}
